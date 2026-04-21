eba4692f99639f925b5330a63767a08cbb586869
sainithin3112 <arshanapallisainithin@gmail.com>
2026-04-21 11:48:29 +0530

    config: update server IP
    Signed-off-by: sainithin3112 <arshanapallisainithin@gmail.com>

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String detApiHost = 'http://192.168.0.131:3000';
-  static const String detWsUrl = 'ws://192.168.0.131:3000';
+  static const String detApiHost = 'http://192.168.0.76:3000';
+  static const String detWsUrl = 'ws://192.168.0.76:3000';
 }
